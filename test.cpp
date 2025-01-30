1f7876208e72cbd17b08f0ce21943bca2ec2d452
Kenny <iik3nnyz11@gmail.com>
2025-01-30 12:13:40 -0500

    Your commit message here

---
+++ test.cpp
@@ -0,0 +1,12 @@
+// File: test.cpp
+// Author: Kenny Zhou
+// Date: 1/30/2025
+// A test file thatt tests our function in HelloWorld.cpp
+
+#include "HelloWorld.hpp"
+#include <iostream>
+
+int main() {
+    HelloWorld();
+    return 0;
+}
